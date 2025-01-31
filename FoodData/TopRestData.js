8dbde8cd1467d15e3e7589def7e561fcde2bf1ad
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-01-31 12:21:58 +0530

    ho

--- FoodData/TopRestData.js
+++ FoodData/TopRestData.js
@@ -14,6 +14,14 @@ const RestData=[
         Time: '30-35 mins',
         FoodCategory: 'Chinese, Asian, Tibetan, Desserts',
         Location: 'South X Mall'
+    },
+    {
+        FoodImage: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/21/42e32ae9-d278-401a-b307-33a0b875c0be_454951.JPG',
+        FoodName: 'Chinese Fork',
+        Rating: '4.2',
+        Time: '30-35 mins',
+        FoodCategory: 'Chinese, Asian, Tibetan, Desserts',
+        Location: 'South X Mall'
     }
 ]
 export default RestData;
