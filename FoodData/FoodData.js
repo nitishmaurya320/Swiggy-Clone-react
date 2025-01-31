423aadb5a6978d5ce3f1150b4665f9616059d135
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-01-31 15:53:17 +0530

    path corrected

--- FoodData/FoodData.js
+++ FoodData/FoodData.js
@@ -1,66 +1,66 @@
 const FoodData=[
     {
-        FoodImage: 'FoodData/FoodImages/Biryani.png',
+        FoodImage: 'FoodImages/Biryani.png',
         FoodName: 'Biryani'
     },
     {
-        FoodImage: './FoodData/FoodImages/Burger.png',
+        FoodImage: 'FoodImages/Burger.png',
         FoodName: 'Burger'
     },
     {
-        FoodImage: '../../FoodData/FoodImages/Cake.png',
+        FoodImage: 'FoodImages/Cake.png',
         FoodName: 'Cake'
     },
     {
-        FoodImage: './FoodImages/Noodles.png',
+        FoodImage: 'FoodImages/Noodles.png',
         FoodName: 'Noodles'
     },
     {
-        FoodImage: '//FoodData./FoodImages./North-Indian.png',
+        FoodImage: 'FoodImages/North-Indian.png',
         FoodName: 'North Indian'
     },
     {
-        FoodImage: '././FoodData/FoodImages/pizza.png',
+        FoodImage: 'FoodImages/pizza.png',
         FoodName: 'Pizza'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Chole-Bhature.png',
+        FoodImage: 'FoodImages/Chole-Bhature.png',
         FoodName: 'Chole Bhature'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Gulab-Jamun.png',
+        FoodImage: 'FoodImages/Gulab-Jamun.png',
         FoodName: 'Gulab Jamun'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Ice-Cream.png',
+        FoodImage: 'FoodImages/Ice-Cream.png',
         FoodName: 'Ice Cream'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Kebabs.png',
+        FoodImage: 'FoodImages/Kebabs.png',
         FoodName: 'Kebabs'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Momos.png',
+        FoodImage: 'FoodImages/Momos.png',
         FoodName: 'Momos'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Noodles.png',
+        FoodImage: 'FoodImages/Noodles.png',
         FoodName: 'Noodles'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Paratha.png',
+        FoodImage: 'FoodImages/Paratha.png',
         FoodName: 'Paratha'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Pasta.png',
+        FoodImage: 'FoodImages/Pasta.png',
         FoodName: 'Pasta'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Pastry.png',
+        FoodImage: 'FoodImages/Pastry.png',
         FoodName: 'Pastry'
     },
     {
-        FoodImage: '././FoodData/FoodImages/Shake.png',
+        FoodImage: 'FoodImages/Shake.png',
         FoodName: 'Shake'
     }
 ]
